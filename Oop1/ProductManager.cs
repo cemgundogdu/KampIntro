f6da7c4460577853b8f061f2047f94c285dec74c
Cem gündoğdu <cem.gundogdu123@gmail.com>
2021-02-11 16:25:18 +0200

    23 Ocak ders sonu

---
+++ Oop1/ProductManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class productManager
+    {
+        public void add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Eklendi. ");
+        }
+
+        internal static void Add(object product, double v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Add(Product product1)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Güncellendi. ");
+        }
+
+      public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+
+
+
+
+
+    }
+
+
+
+}
--- degerVeReferansTipler/Program.cs
+++ degerVeReferansTipler/Program.cs
@@ -10,6 +10,8 @@ namespace degerVeReferansTipler
 
           // referans tip = array , class , interface
 
+
+
         }
     }
 }
